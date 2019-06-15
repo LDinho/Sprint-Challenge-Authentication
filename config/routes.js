4dd8a5feb0710c2acfa0f133404688e3d1a2e956
LD <LDinho@users.noreply.github.com>
2019-06-15 13:45:43 -0400

    [ MVP ] add login route endpoint implementation

--- config/routes.js
+++ config/routes.js
@@ -55,8 +55,52 @@ async function register(req, res) {
   }
 }
 
-function login(req, res) {
-  // implement user login
+async function login(req, res) {
+  const { username, password } = req.body;
+
+  try {
+    if (!username || !password) {
+      return res.status(400)
+        .json({
+          errorMessage: "username or password missing."
+        });
+    }
+
+    const user = await getUserBy({ username });
+
+    if (!user) {
+      return res.status(400)
+        .json({
+          message: `user not found`
+        });
+    }
+
+    const isValidPassword =
+      bcrypt.compareSync(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401)
+        .json({
+          message: 'Invalid Credentials'
+        });
+    } else {
+
+      const token = generateToken(user);
+
+      return res.status(200)
+        .json({
+          message: `Welcome ${user.username}!`,
+          authToken: token,
+        });
+    }
+  }
+  catch (err) {
+    res.status(500)
+      .json({
+        err: err.message,
+        error: `Server error`
+      })
+  }
 }
 
 function getJokes(req, res) {
