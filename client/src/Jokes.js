fa6243ab8d0e9766f0725aa3a23b78d4f4783284
LD <LDinho@users.noreply.github.com>
2019-06-15 15:55:46 -0400

    add working login and signup functionality
    - on success page redirects to jokes page

--- client/src/Jokes.js
+++ client/src/Jokes.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Jokes extends Component {
 
   componentDidMount() {
+    console.log('hello');
 
   }
 
--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,56 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import axios from './helpers/axiosConfig';
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    }
+
+    this.defaultState = this.state;
+
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+
+    })
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const {username, password} = this.state;
+
+    try {
+
+      const result = await axios.post('/login', {
+        username,
+        password,
+      });
+
+      const token = result.data.authToken;
+      localStorage.setItem('token', token);
+
+      this.props.history.push('/jokes');
+
+    }
+    catch (err) {
+      console.error(err);
+
+    }
+
+    this.setState({
+      ...this.defaultState,
+
+    })
+  }
 
   render() {
 
@@ -9,9 +59,25 @@ class Login extends Component {
         <h3 className="login">
           Log in
         </h3>
+
+        <form onSubmit={this.handleSubmit}>
+          <input type="text"
+                 name="username"
+                 placeholder="Username"
+                 value={this.state.username}
+                 onChange={this.handleChange}
+          />
+          <input type="password"
+                 name="password"
+                 placeholder="Password"
+                 value={this.state.password}
+                 onChange={this.handleChange}
+          />
+          <button type="submit">Login</button>
+        </form>
       </>
     );
   }
 }
 
-export default Login;
+export default withRouter(Login);
