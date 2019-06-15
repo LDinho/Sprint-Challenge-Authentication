c405ed118775598849cab0872d0382d88892d9c3
LD <LDinho@users.noreply.github.com>
2019-06-15 16:17:06 -0400

    add rendering of jokes list

--- client/src/Jokes.js
+++ client/src/Jokes.js
@@ -1,22 +1,61 @@
 import React, { Component } from "react";
+import { withRouter } from 'react-router-dom';
+
+
+import axios from './helpers/axiosConfig';
 
 class Jokes extends Component {
 
-  componentDidMount() {
-    console.log('hello');
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      jokes: [],
+    }
+
+  }
+
+  async componentDidMount() {
+
+    try {
+      const result = await axios.get('/jokes');
+
+      this.setState({
+        jokes: result.data,
+      })
+
+    }
+    catch (err) {
+      if (err.response.status === 403 || err.response.status === 401) {
+        this.props.history.push('/login');
+
+      } else {
+
+        console.error(err);
+      }
+
+    }
   }
 
   render() {
 
     return (
       <>
-        <div className="jokes-list">
+        <h2 className="jokes">
           Jokes
-        </div>
+        </h2>
+
+        <ul className="jokes-list">
+          {this.state.jokes.map((joke) => {
+
+            return <li key={joke.id}> {joke.joke} </li>
+
+          })}
+        </ul>
+
       </>
     );
   }
 }
 
-export default Jokes;
+export default withRouter(Jokes);
